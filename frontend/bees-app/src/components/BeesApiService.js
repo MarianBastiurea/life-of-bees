3b0329dd59ac43bf7e184bfeb5a3fc6a65685105
MarianBastiurea <marian.bastiurea@gmail.com>
2026-03-04 10:50:40 +0000

    Revert "Update"
    This reverts commit cc9653ebb481f16ff65b819d023305b00e8e8aec.

--- frontend/bees-app/src/components/BeesApiService.js
+++ frontend/bees-app/src/components/BeesApiService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+
 const apiClient = axios.create({
-  baseURL: "/api",
+    baseURL: API_BASE_URL + '/api',
 });
 
 export const getAuthToken = () => {
